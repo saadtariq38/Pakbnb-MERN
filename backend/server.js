2461a24f0d7589cc8ba38a722789d15c34c41108
saad <saadtariq0812@gmail.com>
2022-12-31 20:29:16 +0500

    added user routes, functions and JSON web token functionality

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended : false }))
 
 
 app.use('/api/rooms', require('./routes/roomRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
