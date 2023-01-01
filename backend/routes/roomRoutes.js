12aa71d80edca6ed8f98e64cd9b1687a0a4fe5d3
saad <saadtariq0812@gmail.com>
2023-01-01 17:38:18 +0500

    added user auth middleware to routes and get user rooms functionality

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const { getRooms, updateRoom, setRoom, deleteRoom, getUserRooms  } = require('../controllers/roomController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getRooms).post(setRoom)
-router.route('/:id').put(updateRoom).delete(deleteRoom)
-router.route('/:userId').get(getUserRooms)
+
+router.route('/').get(protect, getRooms).post(protect, setRoom)
+router.route('/:id').put(protect, updateRoom).delete(protect, deleteRoom)
+router.route('/userRoom').get(protect, getUserRooms)
 
 
 
